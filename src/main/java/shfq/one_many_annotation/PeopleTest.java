11d3eadacef247eaa3d1f042e9e96beb7f89563f
shfq <shanbeirenshfq@163.com>
2016-08-04 22:16:25 +0800

    commit

---
+++ src/main/java/shfq/one_many_annotation/PeopleTest.java
@@ -0,0 +1,33 @@
+package shfq.one_many_annotation;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Created by shfq on 2016/8/4.
+ */
+public class PeopleTest {
+    public static void main(String[] args) {
+        testQuery();
+    }
+    public static void testQuery() {
+        try {
+            Reader reader = Resources.getResourceAsReader("shfq/mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            SqlSession session = sqlSessionFactory.openSession();
+            // query student data
+            Object people = session.selectList("people.selectPeople", 1);
+            System.out.println("record queried successfully");
+            session.commit();
+            session.close();
+            System.out.println("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
