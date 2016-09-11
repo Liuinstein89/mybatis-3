97179470a4f8443317548214996192c003065e26
shfq <shanbeirenshfq@163.com>
2016-09-11 19:06:17 +0800

    multiple resultset add stored procedure

---
+++ src/main/java/shfq/multiple_resultset/xml/storedProcedure.sql
@@ -0,0 +1,11 @@
+DROP PROCEDURE IF EXISTS Proc;
+
+
+delimiter //
+create procedure Proc(in paramId int)
+begin
+	select * from people where id=paramId;
+	select * from pet where owner_id=paramId;
+end //
+delimiter;
+
